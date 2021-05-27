e1beef1ae7eb41ee453e86f6efac69683dff6a38
Terry <choir0118@gmail.com>
2021-05-28 00:10:26 +0900

    feat Hash Level2(2 solution(score : 100/100))

--- intellij/Algorithm/src/thkim/hash/HashMapLevel2.java
+++ intellij/Algorithm/src/thkim/hash/HashMapLevel2.java
@@ -1,10 +1,49 @@
 package thkim.hash;
 
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class HashMapLevel2 {
+
+    public boolean solution___(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+
+        HashMap<String, String> hashMap = new HashMap<>();
+
+        for (String phoneNum : phone_book) {
+            hashMap.put(phoneNum, phoneNum);
+
+            for (int i = 0; i < phoneNum.length(); i++) {
+                String mini = phoneNum.substring(0, i);
+
+                if (hashMap.get(mini) != null) {
+                    answer = false;
+                    break;
+                }
+            }
+
+            if (!answer) break;
+        }
+        return answer;
+    }
+
+    public boolean solution__(String[] phone_book) {
+        boolean answer = true;
+
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
+            }
+        }
+
+        return answer;
+    }
+
     public boolean solution_(String[] phone_book) {
         boolean answer = true;
 
--- intellij/Algorithm/src/thkim/hash/Main.kt
+++ intellij/Algorithm/src/thkim/hash/Main.kt
@@ -1,14 +1,13 @@
 package thkim.hash
 
 fun main() {
-
     println("--------------------------")
-    testHashMapLevel1()
-    testHashMapLevel2()
+    thkim.hash.testHashMapLevel1()
+    thkim.hash.testHashMapLevel2()
 }
 
 fun testHashMapLevel1() {
-    val lv1 = HashMapLevel1()
+    val lv1 = thkim.hash.HashMapLevel1()
 
     println("1. Hash Level 1")
     println(lv1.solution(arrayOf("mislav", "stanko", "mislav", "ana"), arrayOf("stanko", "ana", "mislav")))
@@ -20,7 +19,9 @@ fun testHashMapLevel2() {
     val lv2 = HashMapLevel2()
 
     println("2. Hash Level 1")
-    println(lv2.solution(arrayOf("119", "97674223", "1195524421")))
-    println(lv2.solution(arrayOf("123", "456", "789")))
+    println(lv2.solution___(arrayOf("119", "97674223", "1195524421")))
+//    println(lv2.solution__(arrayOf("119", "97674223", "1195524421")))
+//    println(lv2.solution__(arrayOf("1195524421", "119", "97674223")))
+//    println(lv2.solution(arrayOf("123", "456", "789")))
     println("--------------------------")
 }
