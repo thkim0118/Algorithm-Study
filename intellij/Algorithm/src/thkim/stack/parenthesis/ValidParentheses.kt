9f66beeff9da5628b98de924b1b10a06956f5688
Terry <choir0118@gmail.com>
2021-06-02 22:18:53 +0900

    Add : Stack solution(Valid Parentheses)

--- intellij/Algorithm/src/thkim/stack/parenthesis/ValidParentheses.kt
+++ intellij/Algorithm/src/thkim/stack/parenthesis/ValidParentheses.kt
@@ -43,4 +43,18 @@ class ValidParentheses {
 
         return stack.empty()
     }
+
+    fun isValid_1(s: String): Boolean {
+        val stack = Stack<Char>()
+        s.reversed().forEach {
+            when (it) {
+                '(' -> if (stack.isNotEmpty() && stack.peek() == ')') stack.pop() else stack.push(it)
+                '[' -> if (stack.isNotEmpty() && stack.peek() == ']') stack.pop() else stack.push(it)
+                '{' -> if (stack.isNotEmpty() && stack.peek() == '}') stack.pop() else stack.push(it)
+                else -> stack.push(it)
+            }
+        }
+
+        return stack.isEmpty()
+    }
 }
