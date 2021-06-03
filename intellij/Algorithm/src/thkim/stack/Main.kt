ff6ebf46f3f29e9e6d3d23bce667735a5b147540
Terry <choir0118@gmail.com>
2021-06-03 21:00:16 +0900

    Docs : Update README.md(Stack)

--- intellij/Algorithm/src/thkim/stack/Main.kt
+++ intellij/Algorithm/src/thkim/stack/Main.kt
@@ -1,11 +1,13 @@
 package thkim.stack
 
+import thkim.stack.element.NextGreaterElement2
 import thkim.stack.great.MakeTheStringGreat
 import thkim.stack.parenthesis.ValidParentheses
 
 fun main() {
 //    testMakeTheStringGreat()
-    testValidParentheses()
+//    testValidParentheses()
+    testNextGreaterElement2()
 }
 
 fun testMakeTheStringGreat() {
@@ -25,6 +27,14 @@ fun testValidParentheses() {
     println(parentheses.isValid("([)]")) // false
     println(parentheses.isValid("{[]}")) // true
     println(parentheses.isValid("({[)")) // false
+    println("--------------------------")
+}
+
+fun testNextGreaterElement2() {
+    val element2 = NextGreaterElement2()
+
+    println("Make The String Great")
+    println(element2.nextGreaterElements(intArrayOf(5, 1, 2, 0, 4))) // true
     println("--------------------------")
 
 }
---
+++ intellij/Algorithm/src/thkim/stack/element/NextGreaterElement2.kt
@@ -0,0 +1,16 @@
+package thkim.stack.element
+
+import java.util.*
+
+class NextGreaterElement2 {
+
+    /*
+     * input :  [1, 2, 1],  [1, 2, 3, 4, 3],    [1, 2, 3, 4, 3, 6, 0, 5],   [1,2,3,6,0,5,1,0,1,7]
+     * output : [2, -1, 2], [2, 3, 4, -1, 4],   [2, 3, 4, 6, 6, -1, 5, 6],  [2,3,6,7,5,7,7,1,7,-1]
+     */
+    fun nextGreaterElements(nums: IntArray): IntArray {
+        val answer = arrayListOf<Int>()
+
+        return nums
+    }
+}
