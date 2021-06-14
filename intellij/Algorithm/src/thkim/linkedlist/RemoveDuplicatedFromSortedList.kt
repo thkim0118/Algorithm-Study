e3c13fa00fc2633b2347107de11a9cd84cd5adba
Terry <choir0118@gmail.com>
2021-06-15 00:25:56 +0900

    Add : LinkedList solution(Remove Duplicated From Sorted List)

---
+++ intellij/Algorithm/src/thkim/linkedlist/RemoveDuplicatedFromSortedList.kt
@@ -0,0 +1,52 @@
+package thkim.linkedlist
+
+fun main() {
+
+}
+
+fun deleteDuplicates(head: ListNode?): ListNode? {
+    if (head == null) {
+        return head
+    }
+
+    val answer = ListNode(head.`val`)
+    var nextNode = head.next
+
+    fun recursion(curNode: ListNode) {
+        if (nextNode != null) {
+            if (curNode.`val` == nextNode!!.`val`) {
+                nextNode = nextNode!!.next
+
+                recursion(curNode)
+            } else {
+                curNode.next = nextNode
+
+                recursion(curNode.next!!)
+            }
+        } else {
+            curNode.next = null
+        }
+    }
+
+    recursion(answer)
+
+    return answer
+}
+
+fun deleteDuplicates2(head: ListNode?): ListNode {
+    var node = head
+
+    while (node!!.next != null) {
+        if (node.`val` == node.next!!.`val`) {
+            node.next = node.next!!.next
+        } else {
+            node = node.next
+        }
+    }
+
+    return head!!
+}
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
