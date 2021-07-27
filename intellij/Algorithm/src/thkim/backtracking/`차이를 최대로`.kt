6e5f927cb5718674df80e0a7248c85261616df8d
Terry <choir0118@gmail.com>
2021-07-27 20:08:06 +0900

    Feat : 백준 10819 '차이를 최대로'

---
+++ intellij/Algorithm/src/thkim/backtracking/`차이를 최대로`.kt
@@ -0,0 +1,45 @@
+package thkim.backtracking
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+import kotlin.math.abs
+import kotlin.math.max
+
+// Permutation
+fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val N = readLine().toInt()
+    val arr = IntArray(N)
+    val copyArr = IntArray(N)
+
+    readLine().split(" ").forEachIndexed { idx, data -> arr[idx] = data.toInt() }
+
+    Arrays.sort(arr)
+
+    val visited = BooleanArray(N)
+    var result: Int = Int.MIN_VALUE
+
+    fun permutation(pivot: Int) {
+        if (pivot == N) {
+            var sum = 0
+            for (i in 0 until N - 1) {
+                sum += abs(copyArr[i] - copyArr[i + 1])
+            }
+            result = max(result, sum)
+            return
+        }
+
+        for (i in 0 until N) {
+            if (visited[i]) continue
+
+            visited[i] = true
+            copyArr[pivot] = arr[i]
+            permutation(pivot + 1)
+            visited[i] = false
+        }
+    }
+
+    permutation(0)
+
+    println(result)
+}
