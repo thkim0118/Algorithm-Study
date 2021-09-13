458f52577ba84791d65ba048cdbd4749b0c4a10e
Terry <choir0118@gmail.com>
2021-09-13 23:09:42 +0900

    Feat : 프로그래머스 dfs `단어 변환`

---
+++ intellij/Algorithm/src/thkim/graph/dfs/`단어 변환`.kt
@@ -0,0 +1,41 @@
+package thkim.graph.dfs
+
+fun main() {
+    val result =`단어 변환`().solution("hit", "cog", arrayOf("hot", "dot", "dog", "lot", "log", "cog"))
+    println(result)
+}
+
+class `단어 변환` {
+    fun solution(begin: String, target: String, words: Array<String>): Int {
+        var answer = 0
+
+        val visited = BooleanArray(words.size)
+
+        fun dfs(begin: String, count: Int = 0) {
+            if (begin == target) {
+                answer = count
+                return
+            }
+
+            for ((i, string) in words.withIndex()) {
+                if (visited[i]) continue
+
+                var matchCount = 0
+                for ((j, char) in begin.withIndex()) {
+                    if (char == string[j]) matchCount += 1
+                    if (j - matchCount >= 2) continue
+                }
+
+                if (matchCount == begin.length - 1) {
+                    visited[i] = true
+                    dfs(string, count + 1)
+                    visited[i] = false
+                }
+            }
+        }
+
+        dfs(begin)
+
+        return answer
+    }
+}
