ced612f9a50b7cc7a255a434df914f04bcdd1394
Terry <choir0118@gmail.com>
2021-10-02 16:45:33 +0900

    Feat : 프로그래머스 `K번째수`(Sorting) - Quick sorting

--- intellij/Algorithm/src/thkim/week1003/`K번째수`.kt
+++ intellij/Algorithm/src/thkim/week1003/`K번째수`.kt
@@ -23,10 +23,30 @@ class `K번째수` {
             val i = arr[0]
             val j = arr[1]
             val k = arr[2]
-            answer[index] = array.slice((i - 1) until j).sorted()[k - 1]
+            val sliceArr = array.slice((i - 1) until j).toIntArray()
+            sort(sliceArr, 0, sliceArr.size - 1)
+            answer[index] = sliceArr[k - 1]
         }
 
         return answer
     }
 
+    fun sort(a: IntArray, left: Int, right: Int) {
+        var pl = left
+        var pr = right
+        val x = a[(pl + pr) / 2]
+        do {
+            while (a[pl] < x) pl++
+            while (a[pr] > x) pr--
+            if (pl <= pr) {
+                val temp = a[pl]
+                a[pl] = a[pr]
+                a[pr] = temp
+                pl++
+                pr--
+            }
+        } while (pl <= pr)
+        if (left < pr) sort(a, left, pr)
+        if (right > pl) sort(a, pl, right)
+    }
 }
